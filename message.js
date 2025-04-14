82671b0af36e744c024503bf43f61764848d5533
Nimrod Huberman <cconnect@netvision.net.il>
2025-04-14 08:33:09 +0300

    this.rect1.width = "25%"

--- message.js
+++ message.js
@@ -131,14 +131,14 @@ class Chat {
         this.myWorld = world;
 
         this.advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
-        this.advancedTexture.rootContainer.scaleX = 1 / window.devicePixelRatio;
-        this.advancedTexture.rootContainer.scaleY = 1 / window.devicePixelRatio;   
+        //this.advancedTexture.rootContainer.scaleX = 1 / window.devicePixelRatio;
+        //this.advancedTexture.rootContainer.scaleY = 1 / window.devicePixelRatio;   
 
         this.rect1 = new BABYLON.GUI.Rectangle();
-        this.rect1.width = "500px";
-        this.rect1.height = "600px";
+        this.rect1.width = "25%" //"500px";
+        this.rect1.height = "50%"//"600px";
         this.rect1.cornerRadius = 20;
-        this.rect1.color = "Red"//"Orange";
+        this.rect1.color =  "Orange";
         this.rect1.thickness = 4;
         this.rect1.background = "black";
         this.rect1.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
