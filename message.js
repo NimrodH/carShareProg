fddc5bf70fbc8b66767f3db81582c4e9ac60961a
Nimrod Huberman <cconnect@netvision.net.il>
2025-06-08 07:36:49 +0300

    alloaw rigion all - red

--- message.js
+++ message.js
@@ -579,7 +579,7 @@ class MessageScreen {   //plane = BABYLON.Mesh.CreatePlane("plane2",  { height: 
         this.plane.position.x = 0;
         this.plane.billboardMode = BABYLON.Mesh.BILLBOARDMODE_Y;///without iא its mirror
 
-        this.advancedTexture.background = "green"//green - 'red' for debug color
+        this.advancedTexture.background = "red"//green - 'red' for debug color
         if (showButton) {
             this.nextButton = BABYLON.GUI.Button.CreateSimpleButton("but1", "המשך");
             this.nextButton.width = 1;
